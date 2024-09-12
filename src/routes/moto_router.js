07093c4c1bed3e638d96ead93c0f003603027dcd
João Neto <j.altevirneto@gmail.com>
2024-09-12 09:17:46 -0300

    feat: ajuste de rotas/parametros

--- src/routes/moto_router.js
+++ src/routes/moto_router.js
@@ -4,9 +4,9 @@ const moto_controller = require("../controllers/moto_controllers.js");
 
 router.post("/", moto_controller.store);
 router.get("/", moto_controller.index);
-router.get("/", moto_controller.show);
-router.put("/", moto_controller.update);
-router.delete("/", moto_controller.destroy);
+router.get("/:id", moto_controller.show);
+router.put("/:id", moto_controller.update);
+router.delete("/:id", moto_controller.destroy);
 
 module.exports = router
 
