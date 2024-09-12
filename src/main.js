b28137d435c2f3844487c1b7a719ba504216c77e
João Neto <j.altevirneto@gmail.com>
2024-09-12 09:41:58 -0300

    add rota "refrigerante"

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const moto_router = require("./routes/moto_router.js");
 app.use(express.json());
 
 app.use("/moto", moto_router);
+app.use("refrigetante", refrigerante_router);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
