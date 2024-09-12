8115f1a7dc8697d021d8ad50bf51b7c7d62b23e8
João Neto <j.altevirneto@gmail.com>
2024-09-12 09:04:51 -0300

    feat: apontando a rota de 'moto'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const moto_router = require("./routes/moto_router.js");
 app.use(express.json());
 
-
+app.use("/moto", moto_router);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
