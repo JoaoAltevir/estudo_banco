88bd69156c1c76bc4ddf5be24c60f71cd800e831
João Neto <j.altevirneto@gmail.com>
2024-09-12 08:49:24 -0300

    feat: feito model de moto

---
+++ src/models/moto_model.js
@@ -0,0 +1,31 @@
+const { default: mongoose } = require("mongoose");
+const db = require("../db.js");
+
+const Schema = db.Schema
+
+const motoSchema = new Schema({
+    marca: {
+        type: String,
+        required: true,
+    },
+    cor: {
+        type: String,
+        required: true
+    },
+    modelo: {
+        type: String,
+        required: true
+    },
+    cilindrada: {
+        type: String,
+        required: true
+    },
+    peso: {
+        type: String,
+        required: true
+    }
+})
+
+const Moto = db.model("Moto", motoSchema);
+
+module.exports = Moto
