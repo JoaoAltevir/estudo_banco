eb9a41a34d6ff049d9c0a5bc8e719d34bf1f8559
João Altevir <j.altevirneto@gmail.com>
2024-09-12 22:33:09 -0300

    feito lógica crud aeroporto

---
+++ src/controllers/aeroporto_controller.js
@@ -0,0 +1,54 @@
+import Aeroporto from "../models/aeroporto_model.js"
+
+const store = async (req,res) => {
+    try {
+        await Caneta.create(req.body);
+        res.json();
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+const index = async (req, res) => {
+    try {
+        const content = await Caneta.find().exec();
+        res.json(content);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+const show = async (req,res) => {
+    try {
+        const content = await Caneta.findById(req.params.id).exec();
+        res.json(content); 
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        await Caneta.findByIdAndUpdate(req.params.id, req.body).exec();
+        res.json();    
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+const destroy = async (req, res) => {
+    try {
+        await Caneta.findByIdAndDelete(req.params.id).exec();
+        res.json();
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+export default {
+    store,
+    index,
+    show,
+    update,
+    destroy
+}
