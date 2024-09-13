f9d4c5a14d9b0f68bf7586b480e3ef9d30cca39d
João Altevir <j.altevirneto@gmail.com>
2024-09-12 22:33:40 -0300

    feat: add roteamento para crud de aeroporto

---
+++ src/routes/aeroporto_router.js
@@ -0,0 +1,12 @@
+import cepEndereco from "../middlewares/cepEndereco.js";
+import express from "express";
+const router = express.Router();
+import aeroporto_controller from "../controllers/aeroporto_controller.js";
+
+router.post("/", cepEndereco, aeroporto_controller.store);
+router.get("/", aeroporto_controller.index);
+router.get("/:id", aeroporto_controller.show);
+router.put("/:id", aeroporto_controller.update);
+router.delete("/:id", aeroporto_controller.destroy);
+
+export default router
